02f272e2b6a06defc6f1859f2e6ea96ea68d9d1e
Diego-si <diventura1@gmail.com>
2020-05-15 15:25:21 -0300

    Updates

---
+++ precache-manifest.9380d369e0c0f5e22cdb0881eda7934d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d593eb68a2edf15137537b56479bce39",
+    "url": "/VintageRP/index.html"
+  },
+  {
+    "revision": "0b3bb1380f330d02c5bb",
+    "url": "/VintageRP/static/css/2.f4857a93.chunk.css"
+  },
+  {
+    "revision": "0b3bb1380f330d02c5bb",
+    "url": "/VintageRP/static/js/2.9cbd9e83.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/VintageRP/static/js/2.9cbd9e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf1db7a99397763ffe0",
+    "url": "/VintageRP/static/js/main.bc784b23.chunk.js"
+  },
+  {
+    "revision": "c7f190884b60094e6b18",
+    "url": "/VintageRP/static/js/runtime-main.d0d679f1.js"
+  }
+]);
